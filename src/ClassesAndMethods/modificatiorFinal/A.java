b575856dab2c2173bb8b70b4091d69745acdbc14
Anna <Vasilevaan5@mail.ru>
2024-12-04 03:25:39 +0300

    modificatiorFinal

---
+++ src/ClassesAndMethods/modificatiorFinal/A.java
@@ -0,0 +1,16 @@
+package ClassesAndMethods.modificatiorFinal;
+
+public class A {
+    //1ый
+    public final int a = 1;
+
+//    2ой
+//    public A(int value) {
+//        this.a = value;  // 2nd
+//    }
+
+//    3ий
+//    {
+//        a = 20;
+//    }
+}
