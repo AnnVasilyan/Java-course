0a871a113829da9e9c1bce265b674b3fbf30404d
Anna <Vasilevaan5@mail.ru>
2024-12-06 14:50:54 +0300

    finalle

---
+++ src/exception/blockFinally/Main.java
@@ -0,0 +1,7 @@
+package exception.blockFinally;
+
+public class Main {
+    //try без catch и finally - нельзя, нечего выполнять при exception
+    //try только с finally - можно, при вызове exception выполнится блок finally
+    //try с двумя finally - нельзя, конструкция недопустима.
+}
